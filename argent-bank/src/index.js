d92255dc70b487743bb7c2af80be18b09b56e2ae
flesage-000 <flesage.spam@gmail.com>
2023-02-27 16:36:34 +0100

    Add routing

--- argent-bank/src/index.js
+++ argent-bank/src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 import Signin from './Pages/Signin/Signin';
 import Home from './Pages/Home/Home';
@@ -10,11 +16,22 @@ import User from './Pages/User/User';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <User />
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route  path="/"
+                  element={<Signin />} />
+          <Route  path="/Home"
+                  element={<Home />} />
+          <Route  path="/User"
+                  element={<User />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
