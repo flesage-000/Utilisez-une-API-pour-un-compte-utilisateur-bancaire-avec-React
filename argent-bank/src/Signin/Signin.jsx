632bbc246fa5e41068fe5df2b6567bc06a300799
flesage-000 <flesage.spam@gmail.com>
2023-02-27 18:01:11 +0100

    Add basic form

--- argent-bank/src/Signin/Signin.jsx
+++ argent-bank/src/Signin/Signin.jsx
@@ -9,8 +9,13 @@ const Signin = () => {
 
     const userName = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+
+    // Required forms aren't fill.
+    if (!userName || !password) return;
+
     const rememberMe = document.getElementById('remember-me').checked;
 
+    console.log("login", userName, password, rememberMe)
   }
 
   return (
@@ -22,26 +27,29 @@ const Signin = () => {
         <section className="sign-in-content">
           <i className="fa fa-user-circle sign-in-icon"></i>
           <h1>Sign In</h1>
+
           <form>
+
             <div className="input-wrapper">
               <label htmlFor="username">Username</label>
-              <input type="text" id="username" />
+              <input  type="text"
+                      id="username" />
             </div>
             <div className="input-wrapper">
               <label htmlFor="password">Password</label>
-              <input type="password" id="password" />
+              <input  type="password"
+                      id="password" />
             </div>
             <div className="input-remember">
-              <input type="checkbox" id="remember-me" />
+              <input  type="checkbox"
+                      id="remember-me" />
               <label htmlFor="remember-me">Remember me</label>
             </div>
-            {/* PLACEHOLDER DUE TO STATIC SITE */}
             <button className="sign-in-button"
                     onClick={login}>Sign In</button>
-            {/* SHOULD BE THE BUTTON BELOW */}
-            {/* <button className="sign-in-button">Sign In</button> */}
-            {/*  */}
+
           </form>
+
         </section>
       </main>
 
